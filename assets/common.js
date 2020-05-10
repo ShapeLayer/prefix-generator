d53cf8ce6af385859c4c968b8a8dd27992bcc53a
Park, Jonghyeon <me@ho9.me>
2020-05-11 00:06:21 +0900

    2020-05-10 저녁 정기 수정
    #3 #4 처리 완료

--- assets/common.js
+++ assets/common.js
@@ -4,19 +4,25 @@ function umfinder (value) {
   if (value === '엄') {document.getElementById('supersecret-document').style.display = 'block'}
 }
 
-function joonsikfinder (value) {
+function joonsikfinder (value) { 
+  // 이걸 보고 있는 넌 너무 많은걸 알아버렸어
   var bodyEle = document.getElementsByTagName('body')[0]
   var titleIdentifiers = document.getElementsByClassName('title-identifier')
   if (value === '엄준식') {
-    bodyEle.className = ''
-    bodyEle.classList.add('joonsik')
-    titleIdentifiers[0].innerHTML = '엄준식'
-    titleIdentifiers[1].innerHTML = '엄준식을'
+    //bodyEle.className = ''
+    //bodyEle.classList.add('joonsik')
+    //titleIdentifiers[0].innerHTML = '엄준식'
+    //titleIdentifiers[1].innerHTML = '엄준식을'
   } else if (value === '박원순') {
     bodyEle.className = ''
     bodyEle.classList.add('sibal')
     titleIdentifiers[0].innerHTML = '씨발아저씨'
     titleIdentifiers[1].innerHTML = '씨발아저씨를'
+  } else if (value === '트럼프') {
+    bodyEle.className = ''
+    bodyEle.classList.add('trump')
+    titleIdentifiers[0].innerHTML = '트럼프'
+    titleIdentifiers[1].innerHTML = '트럼프를'
   } else {
     bodyEle.className = ''
     titleIdentifiers[0].innerHTML = '악질 이름'
--- assets/generate.js
+++ assets/generate.js
@@ -10,6 +10,17 @@ function generate (varName, varLocation, targetID) {
   if (Math.random() < 0.8) { /* 일반 장소 처리 */
     var randomDetailLocation = locations[0][Math.floor(Math.random() * locations[0].length)]
     var randomDetailJob = totalJob[Math.floor(Math.random() * totalJob.length)]
+    // 특별한 문장 정의
+    if (randomDetailLocation === '시내버스') {
+      if (Math.random() > 0.3) {
+        randomDetailLocation = (Math.floor(Math.random() * 999)+1) + '번' + randomDetailLocation
+      }
+    } else if (randomDetailLocation.indexOf('학교') >=0) {
+      if (Math.random() > 0.5) {
+        randomDetailLocation = randomDetailLocation + (Math.floor(Math.random() * 2)+1) + '학년' + (Math.floor(Math.random() * 9)+1) + '반'
+      }
+    }
+    //
     if (jobs[0].indexOf(randomDetailJob) >= 0) {
       result = generateSpecificCase(1, [varLocation, randomDetailLocation, randomDetailJob, varName])
     } else if (jobs[1].indexOf(randomDetailJob) >= 0) {
@@ -25,13 +36,19 @@ function generate (varName, varLocation, targetID) {
   }
 
   document.getElementById(targetID).innerHTML = result
-  // debug: console.log(randomDetailLocation, randomDetailJob, result)
+  //debug: 
+  console.log(randomDetailLocation, randomDetailJob, result)
 }
 
 /* params = [varLocation, randomDetailLocation, randomDetailJob, varName] */
 function generateSpecificCase (caseCode, params) {
   if (caseCode === 1) {
     var randomObject = objects[Math.floor(Math.random() * objects.length)]
+    // 특별한 문장 정의
+    if (params[1] === '산채비빔밥먹는스님앞에서') {
+      return params[1] + randomObject + '먹는' + params[3]
+    }
+    //
     return params[0] + params[1] + randomObject + params[2] + params[3]
   } else if (caseCode === 2) {
     return params[0] + params[1] + params[2] + params[3]
@@ -47,5 +64,11 @@ function generateSpecificCase (caseCode, params) {
     }
   } else {
     return undefined
+  }
+}
+
+function debug (a, b, c) {
+  for (i=0; i<100; i++) {
+    generate(a, b, c)
   }
 }
